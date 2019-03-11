16cc53c8f5464028307ceb30a35fdca3538565ea
shivani <shivani.garg.cs.2016@miet.ac.in>
2019-03-11 11:51:00 -0700

    f

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';                //Calculator in reactJS using concept of 'refs'
 import './App.css';
+import './DottedBox.css'
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.myInput=React.createRef();
+    this.input=React.createRef();
+    this.new=React.createRef();
+  };
+  increment=()=>{
+    let num1=parseInt(this.myInput.current.value);
+    let num2=parseInt(this.input.current.value);
+    this.new.current.value=num1+num2;
+  };
+  multiply=()=>{
+    let num1=parseInt(this.myInput.current.value);
+    let num2=parseInt(this.input.current.value);
+    this.new.current.value=num1*num2;
+  };
+  divide=()=>{
+    let num1=parseInt(this.myInput.current.value);
+    let num2=parseInt(this.input.current.value);
+    this.new.current.value=num1/num2;
+  };
+  decrement=()=>{
+    let num1=parseInt(this.myInput.current.value);
+    let num2=parseInt(this.input.current.value);
+    this.new.current.value=num1-num2;
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className='App'>
+          <div className='Body'>
+            <h1><b>CALCULATOR</b></h1>
+
+            <button className='DottedBox_b' onClick={this.increment} ><b><big>ADD</big></b></button>
+            <button className='DottedBox_b' onClick={this.decrement} ><b><big>SUBTRACT</big></b></button>
+            <button className='DottedBox_b' onClick={this.multiply} ><b><big>MULTIPLY</big></b></button>
+            <button className='DottedBox_b' onClick={this.divide} ><b><big>DIVIDE</big></b></button>
+            <br/><br/>
+            <b><input type='text' defaultValue={0} ref={this.myInput} /></b>
+            <b><input type='text' defaultValue={0} ref={this.input} /></b>
+            <b><input type='text' defaultValue={0} ref={this.new} /></b>
+            <br/>
+            <br/>
+
+        </div>
+        </div>
     );
   }
 }
